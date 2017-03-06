f24d1dbb049f1eb865fe3832816724a5a0f268ce
LukasRolle <l.rolle@student.fontys.nl>
2017-03-06 15:20:42 +0100

    Add wearable class

---
+++ ReferenceArchitecture/Implementation/ReferenceArchitecture/src/Wearable/BusinessLogic/Wearable.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Wearable.BusinessLogic;
+
+import Wearable.Interfaces.InputInterface;
+import Wearable.Interfaces.InputText;
+import Wearable.Interfaces.OutputInterface;
+import Wearable.Interfaces.OutputRest;
+import Wearable.Interfaces.OutputText;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Lukas Rolle(LukasRolleSE@gmail.com)
+ */
+public class Wearable {
+    List<InputInterface> inputInterfaces;
+    List<OutputInterface> outputInterfaces;
+    OutputRest networkInterface;
+    
+    List<Process> processes;
+
+    public Wearable() {
+        inputInterfaces = new ArrayList<>();
+        outputInterfaces = new ArrayList<>();
+        processes = new ArrayList<>();
+        
+        OutputText outputText = new OutputText();
+        outputInterfaces.add(outputText);
+        InputText inputText = new InputText();
+        inputInterfaces.add(inputText);
+        LoginProcess loginProcess = new LoginProcess(outputText,inputText);
+        processes.add(loginProcess);
+        initialize();
+    }
+    
+    private void initialize() {
+        processes.get(0).run();
+    }
+}
