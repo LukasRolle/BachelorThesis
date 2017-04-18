1c03e805d5b3d9e113ec388322129785d7db4607
LukasRolle <l.rolle@student.fontys.nl>
2017-04-18 14:53:31 +0200

    Rename Service and Remove xml around json responses

--- WMS/WMS/RestService/IRestService.cs
+++ WMS/WMS/RestService/IRestService.cs
@@ -10,16 +10,16 @@ namespace RestService
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
     [ServiceContract]
-    public interface IService1
+    public interface IRestService
     {
-
+        
         [OperationContract]
         [WebGet(UriTemplate = "Order/?id={id}")]
-        string GetOrder(int id);
-
+        void GetOrder(int id);
+        
         [OperationContract]
         [WebGet(UriTemplate = "NextOrder/?id={id}")]
-        string GetNextOrder(int id);
+        void GetNextOrder(int id);
 
         [OperationContract]
         [WebInvoke(Method = "PATCH", UriTemplate = "ResetDatabase")]
--- WMS/WMS/RestService/RestService.svc.cs
+++ WMS/WMS/RestService/RestService.svc.cs
@@ -7,19 +7,20 @@ using System.ServiceModel.Web;
 using System.Text;
 using DBC;
 using System.Web.Script.Serialization;
-
+using System.Web;
 
 namespace RestService
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
-    public class Service1 : IService1
+    public class RestService : IRestService
     {
-        public string GetOrder(int value)
+        public void GetOrder(int value)
         {
             var serializer = new JavaScriptSerializer();
             var order = Connector.GetOrder(value);
-            return serializer.Serialize(order).ToString();
+            HttpContext.Current.Response.ContentType = "text/HTML";
+            HttpContext.Current.Response.Write(serializer.Serialize(order));
         }
         
         public void ResetDatabase()
@@ -28,10 +29,11 @@ namespace RestService
             helper.ResetDatabase();
         }
 
-        public string GetNextOrder(int id)
+        public void GetNextOrder(int id)
         {
             var serializer = new JavaScriptSerializer();
-            return serializer.Serialize(Connector.GetNextOrder(id));
+            HttpContext.Current.Response.ContentType = "text/HTML";
+            HttpContext.Current.Response.Write(serializer.Serialize(Connector.GetNextOrder(id)));
         }
 
         public void ConfirmOrderLine(int orderId, int orderLineId)
