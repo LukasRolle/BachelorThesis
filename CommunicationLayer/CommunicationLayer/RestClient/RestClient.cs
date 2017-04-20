ccef8195f850f4539a37df6643632847b3fd3801
LukasRolle <l.rolle@student.fontys.nl>
2017-04-20 10:59:38 +0200

    Add general methods to access restservice

--- CommunicationLayer/CommunicationLayer/RestClient/RestClient.cs
+++ CommunicationLayer/CommunicationLayer/RestClient/RestClient.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Threading;
 using System.Web;
 using System.Web.Script.Serialization;
 
@@ -9,10 +10,67 @@ namespace RestClient
 {
     public class RestClient
     {
-        private const string URL = "http://localhost:53268/RestService.svc/NextOrder/";
-        private const string urlParameters = "?id=1";
+        private const string URL = "http://localhost:53268/RestService.svc/";
 
         static void Main(string[] args)
+        {
+            var client = new RestClient();
+            
+
+            for (int j = 0; j < 1000; j++)
+            {
+
+                client.ResetDatabase();
+                for (int i = 1; i < 5; i++)
+                {
+                    var order = client.GetNextOrder(i);
+                    Console.WriteLine("Order number for next order worker number "
+                        + i + ": " + order.OrderNumber);
+                    order = client.GetOrder(i);
+                    Console.WriteLine("Order number for order "
+                        + i + ": " + order.OrderNumber);
+                    Console.WriteLine("Confirmed: " + order.OrderPacked);
+                    foreach (var orderLine in order.OrderLines)
+                    {
+                        client.ConfirmOrderLine(order.OrderNumber, orderLine.OrderLineNumber);
+                    }
+                    client.ConfirmOrder(i);
+                    order = client.GetOrder(i);
+                    Console.WriteLine("Confirmed: " + order.OrderPacked);
+                }
+
+            }
+            Console.ReadKey();
+        }
+
+        public Order GetNextOrder(int workerId)
+        {
+            var response = GetResponse("NextOrder/?id=" + workerId, HttpMethod.Get);
+            return DeserializeJson<Order>(response);
+        }
+        public Order GetOrder(int orderId)
+        {
+            var response = GetResponse("Order/?id=" + orderId, HttpMethod.Get);
+            return DeserializeJson<Order>(response);
+        }
+
+        public void ConfirmOrderLine(int orderId, int orderLineId)
+        {
+            GetResponse("ConfirmOrderLine/?orderId=" + orderId +
+                "&orderLineId=" + orderLineId, HttpMethod.Put);
+        }
+
+        public void ConfirmOrder(int orderId)
+        {
+            GetResponse("ConfirmOrder/?orderId=" + orderId, HttpMethod.Put);
+        }
+
+        public void ResetDatabase()
+        {
+            GetResponse("ResetDatabase", new HttpMethod("PATCH"));
+        }
+
+        private HttpResponseMessage GetResponse(string urlParameters, HttpMethod method)
         {
             HttpClient client = new HttpClient();
             client.BaseAddress = new Uri(URL);
@@ -20,27 +78,23 @@ namespace RestClient
             // Add an Accept header for JSON format.
             client.DefaultRequestHeaders.Accept.Add(
             new MediaTypeWithQualityHeaderValue("application/json"));
-
+            var request = new HttpRequestMessage(method, URL + urlParameters);
             // List data response.
-            HttpResponseMessage response = client.GetAsync(urlParameters).Result;  // Blocking call!
+            HttpResponseMessage response = client.SendAsync(request).Result;  // Blocking call!
             if (response.IsSuccessStatusCode)
             {
-                // Parse the response body. Blocking!
-                Console.WriteLine(response.Content.ReadAsStringAsync().Result);
-                Console.WriteLine("----------");
-                var order = new Order();
-                order = new JavaScriptSerializer().Deserialize<Order>(response.Content.ReadAsStringAsync().Result);
-                Console.WriteLine(order.OrderNumber);
-                Console.ReadLine();
-                //foreach (var d in dataObjects)
-                //{
-                //    Console.WriteLine("{0}", d.OrderNumber);
-                //}
+                return response;
             }
             else
             {
-                Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
+                throw new HttpException("Invalid Status Code");
             }
+        }
+
+        private T DeserializeJson<T>(HttpResponseMessage response)
+        {
+            T instance = new JavaScriptSerializer().Deserialize<T>(response.Content.ReadAsStringAsync().Result);
+            return instance;
         }
     }
 }
