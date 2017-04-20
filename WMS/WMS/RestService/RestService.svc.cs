1a90d43e12cb32b21cf36614d3bddebfe17cf69d
LukasRolle <l.rolle@student.fontys.nl>
2017-04-20 10:59:55 +0200

    Remove pre-existing comments

--- WMS/WMS/RestService/RestService.svc.cs
+++ WMS/WMS/RestService/RestService.svc.cs
@@ -11,8 +11,6 @@ using System.Web;
 
 namespace RestService
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
-    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
     public class RestService : IRestService
     {
         public void GetOrder(int value)
