ec0bc37a2e36c6d048c55f287c925fc6e867e0e1
LukasRolle <l.rolle@student.fontys.nl>
2017-06-22 14:03:29 +0200

    Update version to successfully load model

--- DemoWebApp/DemoFrontEnd/src/main/java/nl/fontys/logwear/demofrontend/Model/Order.java
+++ DemoWebApp/DemoFrontEnd/src/main/java/nl/fontys/logwear/demofrontend/Model/Order.java
@@ -12,24 +12,35 @@ import java.util.List;
  * @author Lukas Rolle(LukasRolleSE@gmail.com)
  */
 public class Order {
+
+    private Customer customer;
     private List<OrderLine> orderLines;
     private List<Worker> workers;
-    private boolean orderPacked;
     private int orderNumber;
-    private Customer customer;
+    private int customerNumber;
+    private boolean orderPacked;
 
-    public Order(List<OrderLine> orderLines, List<Worker> workers, boolean orderPacked, int orderNumber, Customer customer) {
+    public Order(Customer customer, List<OrderLine> orderLines, List<Worker> workers, int orderNumber, int customerNumber, boolean orderPacked) {
         this.customer = customer;
         this.orderLines = orderLines;
         this.workers = workers;
         this.orderNumber = orderNumber;
+        this.customerNumber = customerNumber;
         this.orderPacked = orderPacked;
     }
 
-    public Order() {
-        
+    public int getCustomerNumber() {
+        return customerNumber;
     }
-    
+
+    public void setCustomerNumber(int customerNumber) {
+        this.customerNumber = customerNumber;
+    }
+
+    public Order() {
+
+    }
+
     public Customer getCustomer() {
         return customer;
     }
@@ -69,49 +80,237 @@ public class Order {
     public void setOrderPacked(boolean orderPacked) {
         this.orderPacked = orderPacked;
     }
-    
+
     static class Customer {
-    private int customerNumber;
-    private String customerAddress;
-    private String additionalWishes;
-    
-    public Customer(int customerNumber, String customerAddress, String additionalWishes) {
-        this.customerNumber = customerNumber;
-        this.customerAddress = customerAddress;
-        this.additionalWishes = additionalWishes;
-    }
-    
-    public Customer() {
-        this.customerNumber = 0;
-        this.customerAddress = "";
-        this.additionalWishes = "";
+
+        private int customerNumber;
+        private String customerAddress;
+        private String additionalWishes;
+
+        public Customer(int customerNumber, String customerAddress, String additionalWishes) {
+            this.customerNumber = customerNumber;
+            this.customerAddress = customerAddress;
+            this.additionalWishes = additionalWishes;
+        }
+
+        public Customer() {
+            this.customerNumber = 0;
+            this.customerAddress = "";
+            this.additionalWishes = "";
+        }
+
+        public int getCustomerNumber() {
+            return customerNumber;
+        }
+
+        public void setCustomerNumber(int customerNumber) {
+            this.customerNumber = customerNumber;
+        }
+
+        public String getCustomerAddress() {
+            return customerAddress;
+        }
+
+        public void setCustomerAddress(String customerAddress) {
+            this.customerAddress = customerAddress;
+        }
+
+        public String getAdditionalWishes() {
+            return additionalWishes;
+        }
+
+        public void setAdditionalWishes(String additionalWishes) {
+            this.additionalWishes = additionalWishes;
+        }
+
     }
 
-    public int getCustomerNumber() {
-        return customerNumber;
+    static class OrderLine {
+
+        private int orderLineNumber;
+        private int quantity;
+        private Pallet pallet;
+        private boolean acknowledgement;
+        private int orderNumber;
+        private int palletNumber;
+
+        public OrderLine(int orderLineNumber, int quantity, Pallet pallet, boolean acknowledgement, int orderNumber, int palletNumber) {
+            this.orderLineNumber = orderLineNumber;
+            this.quantity = quantity;
+            this.pallet = pallet;
+            this.acknowledgement = acknowledgement;
+            this.orderNumber = orderNumber;
+            this.palletNumber = palletNumber;
+        }
+
+
+        public OrderLine() {
+
+        }
+
+        public int getPalletNumber() {
+            return palletNumber;
+        }
+
+        public void setPalletNumber(int palletNumber) {
+            this.palletNumber = palletNumber;
+        }
+
+        public int getOrderNumber() {
+            return orderNumber;
+        }
+
+        public void setOrderNumber(int orderNumber) {
+            this.orderNumber = orderNumber;
+        }
+
+        public int getOrderLineNumber() {
+            return orderLineNumber;
+        }
+
+        public void setOrderLineNumber(int orderLineNumber) {
+            this.orderLineNumber = orderLineNumber;
+        }
+
+        public int getQuantity() {
+            return quantity;
+        }
+
+        public void setQuantity(int quantity) {
+            this.quantity = quantity;
+        }
+
+        public Pallet getPallet() {
+            return pallet;
+        }
+
+        public void setPallet(Pallet pallet) {
+            this.pallet = pallet;
+        }
+
+        public boolean isAcknowledgement() {
+            return acknowledgement;
+        }
+
+        public void setAcknowledgement(boolean acknowledgement) {
+            this.acknowledgement = acknowledgement;
+        }
+
+        static class Pallet {
+
+            private String storageLocation;
+            private int articleNumber;
+            private Article article;
+            private int quantity;
+            private int palletNumber;
+
+            
+            public Pallet(String storageLocation, int articleNumber, Article article, int quantity, int palletNumber) {
+                this.storageLocation = storageLocation;
+                this.articleNumber = articleNumber;
+                this.article = article;
+                this.quantity = quantity;
+                this.palletNumber = palletNumber;
+            }
+            
+            public Pallet() {
+
+            }
+
+            public int getArticleNumber() {
+                return articleNumber;
+            }
+
+            public void setArticleNumber(int articleNumber) {
+                this.articleNumber = articleNumber;
+            }
+
+            public String getStorageLocation() {
+                return storageLocation;
+            }
+
+            public void setStorageLocation(String storageLocation) {
+                this.storageLocation = storageLocation;
+            }
+
+            public Article getArticle() {
+                return article;
+            }
+
+            public void setArticle(Article article) {
+                this.article = article;
+            }
+
+            public int getQuantity() {
+                return quantity;
+            }
+
+            public void setQuantity(int quantity) {
+                this.quantity = quantity;
+            }
+
+            public int getPalletNumber() {
+                return palletNumber;
+            }
+
+            public void setPalletNumber(int palletNumber) {
+                this.palletNumber = palletNumber;
+            }
+
+            static class Article {
+
+                private int articleNumber;
+                private String articleName;
+
+                public Article(int articleNumber, String articleName) {
+                    this.articleNumber = articleNumber;
+                    this.articleName = articleName;
+                }
+
+                public Article() {
+
+                }
+
+                public int getArticleNumber() {
+                    return articleNumber;
+                }
+
+                public void setArticleNumber(int articleNumber) {
+                    this.articleNumber = articleNumber;
+                }
+
+                public String getArticleName() {
+                    return articleName;
+                }
+
+                public void setArticleName(String articleName) {
+                    this.articleName = articleName;
+                }
+
+            }
+
+        }
     }
 
-    public void setCustomerNumber(int customerNumber) {
-        this.customerNumber = customerNumber;
-    }
+    static class Worker {
 
-    public String getCustomerAddress() {
-        return customerAddress;
-    }
+        int workerNumber;
+        List<Order> orders;
 
-    public void setCustomerAddress(String customerAddress) {
-        this.customerAddress = customerAddress;
-    }
+        public Worker(int workerNumber) {
+            this.workerNumber = workerNumber;
+        }
 
-    public String getAdditionalWishes() {
-        return additionalWishes;
-    }
+        public Worker() {
 
-    public void setAdditionalWishes(String additionalWishes) {
-        this.additionalWishes = additionalWishes;
-    }
-    
-    
-}
+        }
 
+        public int getWorkerNumber() {
+            return workerNumber;
+        }
+
+        public void setWorkerNumber(int workerNumber) {
+            this.workerNumber = workerNumber;
+        }
+    }
 }
--- DemoWebApp/DemoFrontEnd/src/main/java/nl/fontys/logwear/demofrontend/WorkerBean.java
+++ DemoWebApp/DemoFrontEnd/src/main/java/nl/fontys/logwear/demofrontend/WorkerBean.java
@@ -9,6 +9,7 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationConfig;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -22,6 +23,7 @@ import java.util.logging.Logger;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 import nl.fontys.logwear.demofrontend.Model.Order;
+import org.apache.commons.lang3.text.WordUtils;
 
 /**
  *
@@ -46,9 +48,9 @@ public class WorkerBean implements Serializable {
             Logger.getLogger(WorkerBean.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
+
     public int getWorkerID() {
-        
+
         return workerID;
     }
 
@@ -62,10 +64,10 @@ public class WorkerBean implements Serializable {
         String orderJson = "";
         try {
             restUrl = new URL(baseUrl.toString() + workerID);
-       
+
             connection = (HttpURLConnection) restUrl.openConnection();
             connection.setRequestMethod("GET");
-            
+
             //Get Response  
             InputStream is = connection.getInputStream();
             BufferedReader rd = new BufferedReader(new InputStreamReader(is));
@@ -77,7 +79,8 @@ public class WorkerBean implements Serializable {
             }
             rd.close();
             orderJson = response.toString();
-            //orderJson = "{\"Order\":" + orderJson + "}";
+            orderJson = jsonToCamelCase(orderJson);
+            orderJson = "{\"Order\":" + orderJson + "}";
             System.out.println("Have been here");
         } catch (JsonMappingException ex) {
             Logger.getLogger(WorkerBean.class.getName()).log(Level.SEVERE, null, ex);
@@ -88,11 +91,11 @@ public class WorkerBean implements Serializable {
                 connection.disconnect();
             }
         }
-        
+
         if (!orderJson.equals("")) {
             ObjectMapper mapper = new ObjectMapper();
             mapper.configure(DeserializationFeature.UNWRAP_ROOT_VALUE, true);
-            
+
             try {
                 currentOrder = mapper.readValue(orderJson, Order.class);
             } catch (JsonMappingException ex) {
@@ -103,9 +106,23 @@ public class WorkerBean implements Serializable {
         }
         return currentOrder;
     }
-    
+
     public void setCurrentOrder(Order currentOrder) {
         this.currentOrder = currentOrder;
+    }
+
+    public String jsonToCamelCase(String input) {
+        StringBuilder stringBuilder = new StringBuilder(input);
+        for (int i = 0; i < (input.length() - 1); i++) {
+            char c = input.charAt(i);
+            if (c == '"') {
+                char nextChar = input.charAt(i + 1);
+                if (nextChar >= 65 && nextChar <= 90) {
+                    stringBuilder.setCharAt(i + 1, (char) (nextChar + 32));
+                }
+            }
+        }
+        return stringBuilder.toString();
     }
 
 }
